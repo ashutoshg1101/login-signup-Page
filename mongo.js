e2d920069860e84ec29a230020d9b1ff4a2c7fb0
Aashutosh <ashutoshg1101@gmail.com>
2024-01-17 11:18:28 +0530

    1

---
+++ mongo.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose")
+// react-login-tut
+mongoose.connect("mongodb://0.0.0.0:27017/email-pass")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+
+const newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports=collection
